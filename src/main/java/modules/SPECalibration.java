7fe44f39c12f0f13edd7609a1d384737f5688f8b
bduran <bduran@jlab.org>
2017-11-09 18:22:33 -0500

    Whichever segment you click, 6 histograms will be drawn for the next 6 segments on the same canvas.

--- src/main/java/modules/SPECalibration.java
+++ src/main/java/modules/SPECalibration.java
@@ -22,6 +22,7 @@ import org.jlab.groot.math.F1D;
 import org.jlab.groot.data.GraphErrors;
 import org.jlab.groot.fitter.DataFitter;
 import org.jlab.utils.groups.IndexedList;
+import java.util.Scanner;
 
 
 /**
@@ -44,19 +45,24 @@ public class SPECalibration extends CalibrationModule {
                 for(int iComp=1; iComp<=this.getSegments(); iComp++) { 
                     // initialize calibration table
                     this.getCalibrationTable().addEntry(iSect, iLay, iComp);
-                    //getCalibrationTable().setDoubleValue(0., "offset", iSect, iLay, iComp);
-                    //getCalibrationTable().setDoubleValue(0., "offset_error", iSect, iLay, iComp);
-                    //getCalibrationTable().setDoubleValue(1., "resolution", iSect, iLay, iComp);
+                  
      
                     // initialize data group
-                    H1F speADC = new H1F("speADC_" + iSect + "_" + iLay + "_" + iComp, 100, 0.0, 5000);
+                    H1F speADC = new H1F("speADC_" + iSect + "_" + iLay + "_" + iComp, 100, 100.0, 300.0);
                     speADC.setTitleX("ADC");
                     speADC.setTitleY("counts");
                     speADC.setTitle("spe ADC Channel (" + iSect + "," + iLay + "," + iComp +")");
                     speADC.setFillColor(3);
+                    F1D fADC = new F1D("fADC_"+ iSect + "_" + iLay + "_" + iComp, "[amp]*gaus(x,[mean],[sigma])", 120, 270);
+                    fADC.setParameter(0, 0.0);
+                    fADC.setParameter(1, 0.0);
+                    fADC.setParameter(2, 20.0);
+                    fADC.setLineColor(2);
+                    fADC.setLineWidth(2);
                     
-                    DataGroup dg = new DataGroup(2,2);
-                    dg.addDataSet(speADC, (iComp-1)%4);
+                    DataGroup dg = new DataGroup(3,2);
+                    dg.addDataSet(speADC, (iComp-1)%6);
+                    dg.addDataSet(fADC, (iComp-1)%6);
                     this.getDataGroup().add(dg,iSect, iLay, iComp);
                 }
             }
@@ -87,33 +93,11 @@ public class SPECalibration extends CalibrationModule {
                 int component = bank.getShort("component", loop);
                 int adc = bank.getInt("ADC", loop);
                
-                if(sector>0 && layer>0  && component>0) {
+                if(sector>0 && layer>0  && component>0 && adc>0) {
                 this.getDataGroup().getItem(sector,layer,component).getH1F("speADC_" + sector + "_" + layer + "_" + component).fill(adc);
                 }
             }
         }
-    }
-   
-    public void processShape(DetectorShape2D dsd) {
-        // plot histos for the specific component
-        int sector = dsd.getDescriptor().getSector();
-        int layer  = dsd.getDescriptor().getLayer();
-        int paddle = dsd.getDescriptor().getComponent();
-        System.out.println("Selected shape " + sector + " " + layer + " " + paddle);
-        IndexedList<DataGroup> group = this.getDataGroup();        
-        
-        
-        if(group.hasItem(sector,layer,paddle)==true){
-            if((paddle-1)%4==0){
-                        this.getCanvas().clear();
-            }     
-            //this.getCanvas().clear();
-            this.getCanvas().draw(this.getDataGroup().getItem(sector,layer,paddle));
-            this.getCanvas().update();
-            
-        } else {
-            System.out.println(" ERROR: can not find the data group");
-        }       
     }
     
     public void analyze() {
@@ -122,12 +106,56 @@ public class SPECalibration extends CalibrationModule {
         for (int iSect : this.getDetector().getSectors()) {
             for(int iLay=1; iLay<=this.getLayers(); iLay++) {
                 for(int iComp=1; iComp<=this.getSegments(); iComp++) { 
-                    H1F speADC = this.getDataGroup().getItem(iSect,iLay,iComp).getH1F("speADC_" + iSect + "_" + iLay + "_" + iComp);              
+                    H1F speADC = this.getDataGroup().getItem(iSect,iLay,iComp).getH1F("speADC_" + iSect + "_" + iLay + "_" + iComp);
+                    /*F1D fADC = this.getDataGroup().getItem(iSect,iLay,iComp).getF1D("fADC_" + iSect + "_" + iLay + "_" + iComp);
+                    this.initADCgaussFitPar(fADC, speADC);
+                    DataFitter.fit(fADC, speADC, "LQ");*/
                 }
             }
         }
         getCalibrationTable().fireTableDataChanged();
     }
+    
+        @Override
+        public void processShape(DetectorShape2D dsd) {
+        //plot histos for the specific component
+        int sector = dsd.getDescriptor().getSector();
+        int layer  = dsd.getDescriptor().getLayer();
+        int paddle = dsd.getDescriptor().getComponent();
+        System.out.println("Selected shape " + sector + " " + layer + " " + paddle);
+        IndexedList<DataGroup> group = this.getDataGroup();        
+        
+        if(group.hasItem(sector,layer,paddle)==true){
+            if(paddle%6==0){
+                this.getCanvas().clear();
+            } 
+           
+            int segN = paddle;
+                for(paddle=segN; paddle < segN + 6; paddle++) { 
+                    this.getCanvas().divide(3,2);
+                    this.getCanvas().cd((paddle-segN)%6);
+                    this.getCanvas().draw(this.getDataGroup().getItem(sector,layer,paddle).getH1F("speADC_"+ sector + "_" + layer + "_" + paddle));
+                   // this.getCanvas().draw(this.getDataGroup().getItem(sector,layer,paddle).getF1D("fADC_" + sector + "_" + layer + "_" + paddle),"same");
+                }
+            
+            
+        } else {
+            System.out.println(" ERROR: can not find the data group");
+       }
+         
+   }
+        
+        /*private void initADCgaussFitPar(F1D fADC, H1F speADC) {
+        
+        fADC.setRange(120, 270);
+        fADC.setParameter(0, 15);
+        //fADC.setParLimits(0, -46, 46);
+        fADC.setParameter(1, 200);
+        //fADC.setParLimits(1, -600, 600);
+        fADC.setParameter(2, 20);
+        //fADC.setParLimits(2, 0, 200);
+    }*/
+       
     
     @Override
     public Color getColor(DetectorShape2D dsd) {
