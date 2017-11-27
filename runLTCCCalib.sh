12d508f9810a474e02eef98b3c8c1c14b0b2dc4f
bduran <bduran@jlab.org>
2017-11-27 17:24:39 -0500

    fixed the name of the main class

--- runLTCCCalib.sh
+++ runLTCCCalib.sh
@@ -9,4 +9,4 @@ echo "COATJAVA DIRECTORY = " $CLAS12DIR
 echo "LIBRARY DIRECTORY  = " $DISTRO_DIR/lib
 
 
-java -Dsun.java2d.pmoffscreen=false -Xmx2048m -Xms1024m -cp "$CLAS12DIR/lib/clas/*:$CLAS12DIR/lib/services/*:$DISTRO_DIR/lib/LTCCCalib-1.0.jar" org.clas.viewer.CalibrationViewer $*
+java -Dsun.java2d.pmoffscreen=false -Xmx2048m -Xms1024m -cp "$CLAS12DIR/lib/clas/*:$CLAS12DIR/lib/services/*:$DISTRO_DIR/lib/LTCCCalib-1.0.jar" viewer.CalibrationViewer $*
