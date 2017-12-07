e47a188c2039aa5f83eab28a653f67beac6d84f3
bduran <bduran@jlab.org>
2017-12-07 10:28:56 -0500

    fixed getF1D issue but can not retrieve the fitpar histogram to save the fit parameters

--- src/main/java/modules/SPECalibration.java
+++ src/main/java/modules/SPECalibration.java
@@ -40,7 +40,6 @@ public class SPECalibration extends CalibrationModule {
     public void resetEventListener() {
 
         nevents = 0;
-        double[] par = new double[5];
 
         for (int iSect : this.getDetector().getSectors()) {
             // order indicate left (0) / right (1)
@@ -55,49 +54,22 @@ public class SPECalibration extends CalibrationModule {
                     speADC.setTitleY("counts");
                     speADC.setTitle("spe ADC Channel (" + iSect + "," + iOrde + "," + iComp + ")");
                     speADC.setFillColor(3);
-		    // Exponential function with 2 parameters
-                    F1D fADCe = new F1D("fADCe_" + iSect + "_" + iOrde + "_" + iComp, "exp(x,par[3],par[4])", 100, 3000);;
-                    fADCe.setParameter(3, 0.0);
-                    fADCe.setParameter(4, 0.0);
-                    fADCe.setLineColor(1);
+		    // Exponential fit function with 2 parameters
+                    F1D fADCe = new F1D("fADCe_" + iSect + "_" + iOrde + "_" + iComp, "exp(x,par[0],par[1])", 100, 3000);;
+                    fADCe.setParameter(1, 100.0);
+                    fADCe.setParameter(2, 0.1);
+                    fADCe.setLineColor(2);
                     fADCe.setLineWidth(2);
-		    // Poissonian function defined in poissonf.java with 3 parameters
-	   	    poissonf fADCp = new poissonf("fADCp_" + iSect + "_" + iOrde + "_" + iComp, 100, 2000);
-		    fADCp.setParameter(0, 0.0);
-		    fADCp.setParameter(1, 0.0);
-		    fADCp.setParameter(2, 0.0);
-                    fADCp.setLineColor(2);
-                    fADCp.setLineWidth(2);
-	            // Poissonian + exponential fit function defined in poissonExpo.java with 5 parameters
-                    poissonExpo fADC = new poissonExpo("fADC_" + iSect + "_" + iOrde + "_" + iComp, 100, 3000);
-		    fADC.setParameter(0, 0.0);
-		    fADC.setParameter(1, 0.0);
-		    fADC.setParameter(2, 0.0);
-		    fADC.setParameter(3, 0.0);
-		    fADC.setParameter(4, 0.0);
-                    fADC.setLineColor(3);
-                    fADC.setLineWidth(2);
+		    
+                    
                     //saving the fit parameter in histogram bins
-                    H1F fitpar = new H1F("fitpar_" + iSect + "_" + iOrde + "_" + iComp, 5, 100.0, 3000.0);
-                    fitpar.setBinContent(1, par[0]);
-                    fitpar.setBinContent(2, par[1]);
-                    fitpar.setBinContent(3, par[2]);
-                    fitpar.setBinContent(4, par[3]);
-                    fitpar.setBinContent(5, par[4]);
+                    H1F fitpar = new H1F("fitpar_" + iSect + "_" + iOrde + "_" + iComp, 5, 0.0, 5.0);
                     //fit parameters from DB
-                    H1F fitparDB = new H1F("fitparDB_" + iSect + "_" + iOrde + "_" + iComp, 5, 100.0, 3000.0);
-                    fitparDB.setBinContent(1, par[0]);
-                    fitparDB.setBinContent(2, par[1]);
-                    fitparDB.setBinContent(3, par[2]);
-                    fitparDB.setBinContent(4, par[3]);
-                    fitparDB.setBinContent(5, par[4]);
-
-
+                    H1F fitparDB = new H1F("fitparDB_" + iSect + "_" + iOrde + "_" + iComp, 5, 0.0, 5.0);
+                   
                     DataGroup dg = new DataGroup(3, 2);
                     dg.addDataSet(speADC, (iComp - 1) % 6);
-		    dg.addDataSet(fADC, (iComp - 1) % 6);//added fit func to dg
-                    dg.addDataSet(fADCe, (iComp - 1) % 6); //added expo func to dg
- 		    dg.addDataSet(fADCp, (iComp - 1) % 6); //added poisson func to dg
+                    dg.addDataSet(fADCe, (iComp - 1) % 6); //added expo func to do
                     dg.addDataSet(fitpar, (iComp - 1) % 6);//added fit parameters histo to the datagroup
                     dg.addDataSet(fitparDB, (iComp - 1) % 6);//added histo. of the fit parameters from DB to the datagroup
                     this.getDataGroup().add(dg, iSect, iOrde, iComp);
@@ -150,12 +122,28 @@ public class SPECalibration extends CalibrationModule {
             for (int iOrde = 0; iOrde < 1; iOrde++) {
                 for (int iComp = 1; iComp <= this.getSegments(); iComp++) {
                     H1F speADC = this.getDataGroup().getItem(iSect, iOrde, iComp).getH1F("speADC_" + iSect + "_" + iOrde + "_" + iComp);
-                    F1D fADCe = this.getDataGroup().getItem(iSect, iOrde ,iComp).getF1D("fADCe_" + iSect + "_" + iOrde + "_" + iComp);
-                    //getF1D doesn't work for the poissonf and poissonExpo
-		    poissonf fADCp = this.getDataGroup().getItem(iSect, iOrde ,iComp).getF1D("fADCp_" + iSect + "_" + iOrde + "_" + iComp);
-                    poissonExpo fADC = this.getDataGroup().getItem(iSect, iOrde ,iComp).getF1D("fADCp_" + iSect + "_" + iOrde + "_" + iComp);
-                    //this.initADCFitPar(fADC, speADC); 
-                    //DataFitter.fit(fADC, speADC, "LQ");
+                    // Poissonian fit function defined in poissonf.java with 3 parameters
+	   	    poissonf fADCp = new poissonf("fADCp_" + iSect + "_" + iOrde + "_" + iComp, 100, 2000);
+		    fADCp.setParameter(1, 1000.0);
+		    fADCp.setParameter(2, 4.0);
+		    fADCp.setParameter(3, 40.0);
+                    fADCp.setLineColor(1);
+                    fADCp.setLineWidth(2);
+                    poissonExpo fADC = new poissonExpo("fADC_" + iSect + "_" + iOrde + "_" + iComp, 100, 3000);
+                    fADC.setParameter(1, 1000.0);
+		    fADC.setParameter(2, 4.0);
+		    fADC.setParameter(3, 40.0);
+		    fADC.setParameter(4, 100.0);
+		    fADC.setParameter(5, 0.1);
+                    fADC.setLineColor(1);
+                    fADC.setLineWidth(2);
+                    DataFitter.fit(fADC, speADC, "LQ");
+                    
+                    fitpar.setBinContent(1, fADC.getParameter(1));
+                    fitpar.setBinContent(2, fADC.getParameter(2));
+                    fitpar.setBinContent(3, fADC.getParameter(3));
+                    fitpar.setBinContent(4, fADC.getParameter(4));
+                    fitpar.setBinContent(5, fADC.getParameter(5));
                 }
             }
         }
@@ -194,17 +182,7 @@ public class SPECalibration extends CalibrationModule {
         }
 
     }
-
-    /*private void initADCFitPar(F1D fADCe, H1F speADC) {
-	        
-        fADC.setRange(0, 2000);
-        fADC.setParameter(0, -0.0003);
-        //fADC.setParLimits(0, -46, 46);
-        fADC.setParameter(1, 1.6);
-        //fADC.setParLimits(1, -600, 600);
-        fADC.setParameter(1, 200);
-     
-    }*/
+    
     @Override
     public Color getColor(DetectorShape2D dsd) {
         // show summary
