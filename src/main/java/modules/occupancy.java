23d8ba46c221b46c8040eea79c828a18869a9eca
burcuduran <burcuduran@100.64.33.171>
2018-01-31 02:31:24 -0500

    Channel vs Occupancy plots for each sector

--- src/main/java/modules/occupancy.java
+++ src/main/java/modules/occupancy.java
@@ -13,6 +13,7 @@ import viewer.CalibrationModule;
 import viewer.CCDetector;
 import org.jlab.detector.calib.utils.CalibrationConstants;
 import org.jlab.groot.base.ColorPalette;
+import org.jlab.groot.data.H1F;
 import org.jlab.groot.data.H2F;
 import org.jlab.groot.group.DataGroup;
 import org.jlab.io.base.DataBank;
@@ -38,92 +39,72 @@ public class occupancy extends CalibrationModule {
         nevents = 0;
 
         for (int iSect : this.getDetector().getSectors()) {
-            // order indicate left (0) / right (1)
-            for (int iOrde = 0; iOrde < 2; iOrde++) {
-                for (int iComp = 1; iComp <= this.getSegments(); iComp++) {
-
-                    // initialize calibration table
-                    this.getCalibrationTable().addEntry(iSect, iOrde, iComp);
-
-                    // 19 is right side
-                    // in the past it was 1 = 1 left, 2 = 1 right, etc
-                    int pmtIndex = (iOrde * 18) + iComp;
-
-                    // initialize data group
-                    H2F chanADC = new H2F("chanADC_" + iSect + "_" + pmtIndex, 100, 100.0, 3000.0, 1, 1, 36);
-                    chanADC.setTitleX("ADC" + " " + "Sector" + "_" + iSect);
-                    chanADC.setTitleY("channel");
-
-                    DataGroup dg = new DataGroup(3, 2);
-                    dg.addDataSet(chanADC, (iSect - 1) % 6);
-                    this.getDataGroup().add(dg, iSect, iOrde, iComp);
+            // initialize data group
+            H2F chanADC = new H2F("chanADC_" , 60, 100.0, 3000.0, 18, 0.5, 36.5);
+            chanADC.setTitleX("Sector" + "_" + iSect + " " + "ADC");
+            chanADC.setTitleY("Sector" + "_" + iSect + " " + "channel");
+                  
+            DataGroup dg = new DataGroup(3, 2);
+            dg.addDataSet(chanADC, (iSect - 1));
+            this.getDataGroup().add(dg, iSect, 0, 0);
                 }
             }
-        }
-        getCalibrationTable().fireTableDataChanged();
-    }
-
-    @Override
-    public List<CalibrationConstants> getCalibrationConstants() {
-        return Arrays.asList(getCalibrationTable());
-    }
-
+    
     public int getNEvents(int isec, int order, int icomp) {
+        // order indicate left (0) / right (1)
+        // 1-18 left side; 19-36 right side
+        // in the past it was 1 = 1 left, 2 = 1 right, etc
             int pmtIndex = (order * 18) + icomp;
-            return this.getDataGroup().getItem(isec, order, icomp).getH2F("chanADC_"+ isec + "_" + pmtIndex).getEntries();    
+            return this.getDataGroup().getItem(isec, 0, 0).getH2F("chanADC_").getEntries();    
     }
+    
     public int nevents;
 
     @Override
     public void processEvent(DataEvent event) {
         nevents++;
-        //System.out.println(nevents);
+        
         if (event.hasBank("LTCC::adc") == true) {
             DataBank bank = event.getBank("LTCC::adc");
-            int rows = bank.rows();
-            for (int loop = 0; loop < rows; loop++) {
-                int sector = bank.getByte("sector", loop);
-                int order  = bank.getByte("order", loop);
-                int component = bank.getShort("component", loop);
-                int adc = bank.getInt("ADC", loop);
-                
-                    for(int iSect : this.getDetector().getSectors()){
-                        for(order = 0; order < 2; order++){
-                            for(component = 1; component <= this.getSegments(); component++){
-                               if (sector > 0 && adc > 0) {
-                                    int pmtIndex = (order * 18) + component;
-                                           
-                                    this.getDataGroup().getItem(sector, order, component).getH2F("chanADC_" + sector + "_" + pmtIndex).fill(adc, pmtIndex);
-                            }
-                        }
-                    }
+            int nhits = bank.rows();
+            for (int hitNum = 0; hitNum < nhits; hitNum++) {
+                int sector = bank.getByte("sector", hitNum);
+                int order  = bank.getByte("order", hitNum);
+                int component = bank.getShort("component", hitNum);
+                int adc = bank.getInt("ADC", hitNum) ;
+                int pmtIndex = (order * 18) + component;
+       
+                    if (adc > 0 ) {  
+                        this.getDataGroup().getItem(sector, 0, 0).getH2F("chanADC_").fill(adc, pmtIndex);
+                        System.out.println("filling : " + sector + " " + order + " " + component);
+                   }
+                           
                 }
             }
         }
-    }   
-    
+   
     public void analyze() {
         System.out.println("Analyzing");
 
         for (int iSect : this.getDetector().getSectors()) {
             // order indicate left (0) / right (1)
+            // 1-18 left side; 19-36 right side
+            // in the past it was 1 = 1 left, 2 = 1 right, etc
             for (int iOrde = 0; iOrde < 2; iOrde++) {
                 for (int iComp = 1; iComp <= this.getSegments(); iComp++) {
                     int pmtIndex = (iOrde * 18) + iComp;
-
-                    H2F chanADC = this.getDataGroup().getItem(iSect, iOrde, iComp).getH2F("chanADC_" + iSect + "_" + pmtIndex);
+                    H2F chanADC = this.getDataGroup().getItem(iSect, 0, 0).getH2F("chanADC_");
                 }
             }
         }
-        getCalibrationTable().fireTableDataChanged();
     }
-
+    
     @Override
     public void processShape(DetectorShape2D dsd) {
 
-        //plot histos for the specific component
+        //plot histos for each sector
         int sector = dsd.getDescriptor().getSector();
-        // layer is 1, 2 in the detector shape. It is 0, 1, in the histo (order
+        // layer is 1, 2 in the detector shape. It is 0, 1, in the histo order
         int layer = dsd.getDescriptor().getLayer();
         int order = layer - 1;
         int paddle = dsd.getDescriptor().getComponent();
@@ -131,22 +112,23 @@ public class occupancy extends CalibrationModule {
         System.out.println("Selected shape " + sector + " " + pmtIndex);
         IndexedList<DataGroup> group = this.getDataGroup();
 
-        if (group.hasItem(sector, order, paddle) == true) {
-            if (sector % 6 == 0) {
+        if (group.hasItem(sector, 0, 0) == true) {
+            if (sector  == 6) {
                 this.getCanvas().clear();
             }
-
-            for (int sect : this.getDetector().getSectors()) {
+            
+            for (sector = 1; sector <= 6; sector++) {
                 this.getCanvas().divide(3, 2);
-                this.getCanvas().cd((sect - 1) % 6);
-                this.getCanvas().draw(this.getDataGroup().getItem(sect, order, paddle).getH2F("chanADC_" + sect + "_" + pmtIndex));
+                this.getCanvas().cd((sector - 1));
+                this.getCanvas().getPad(sector-1).getAxisZ().setLog(true);
+                this.getCanvas().draw(this.getDataGroup().getItem(sector, 0, 0).getH2F("chanADC_"));
+                
             }
         }else {
             System.out.println(" ERROR: can not find the data group");
         }
-
-    }
-
+            }
+   
     @Override
     public Color getColor(DetectorShape2D dsd) {
         // show summary
@@ -156,12 +138,11 @@ public class occupancy extends CalibrationModule {
         int key = dsd.getDescriptor().getComponent();
         ColorPalette palette = new ColorPalette();
         Color col = new Color(100, 100, 100);
-        int nent = this.getNEvents(sector, order, key);
+        int nent = this.getNEvents(sector, 0, 0);
         if (nent > 0) {
             col = palette.getColor3D(nent, this.getnProcessed(), true);
         }
 
-//        col = new Color(100, 0, 0);
         return col;
     }
     @Override
