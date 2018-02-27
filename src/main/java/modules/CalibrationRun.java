e9fbaaa5fd6f0ecc7da1dc2278b1c758d6058f2d
Maurizio Ungaro <maurizio.ungaro@cnu.edu>
2018-02-27 11:49:41 -0500

    temp commit, fearing desktop freeze

--- src/main/java/modules/CalibrationRun.java
+++ src/main/java/modules/CalibrationRun.java
@@ -1,7 +1,9 @@
 package modules;
 
 import org.jlab.detector.calib.utils.CalibrationConstants;
+import org.jlab.groot.data.H1F;
 import org.jlab.groot.group.DataGroup;
+import org.jlab.groot.math.F1D;
 import org.jlab.utils.groups.IndexedList;
 
 public class CalibrationRun {
@@ -54,20 +56,51 @@ public class CalibrationRun {
             for (int d = 0; d < NSIDES; d++) {
                 for (int p = 0; p < NSEGMENTS; p++) {
 
+                    H1F allFadc = new H1F(nameForObject(FADC_ALLNAME, s, d, p), 200, 0.0, 1000.0);
+                    H1F eleFadc = new H1F(nameForObject(FADC_ELENAME, s, d, p), 200, 0.0, 4000.0);
+                    H1F pioFadc = new H1F(nameForObject(FADC_PIONAME, s, d, p), 200, 0.0, 4000.0);
+                    H1F rndFadc = new H1F(nameForObject(FADC_RNDNAME, s, d, p), 200, 0.0, 1000.0);
+
+                    allFadc.setTitleX("ADC");
+                    allFadc.setTitleY("Counts");
+                    allFadc.setTitle(titleForObject("all ADC", s, d, p));
+                    eleFadc.setTitleX("ADC");
+                    eleFadc.setTitleY("Counts");
+                    eleFadc.setTitle(titleForObject("electrons ADC", s, d, p));
+                    pioFadc.setTitleX("ADC");
+                    pioFadc.setTitleY("Counts");
+                    pioFadc.setTitle(titleForObject("pions ADC", s, d, p));
+                    rndFadc.setTitleX("ADC");
+                    rndFadc.setTitleY("Counts");
+                    rndFadc.setTitle(titleForObject("rnd trigger ADC", s, d, p));
+
+                  
+//                    DataGroup dg = new DataGroup(6, 3);
+//                    dg.addDataSet(speADC, 0);
+//                    dg.addDataSet(fitpars, 0);   // added fit parameters histo to the datagroup
+//                    dg.addDataSet(fitparsDB, 0);   // added hist containing fit parameters from DB to the datagroup
+//                    dg.addDataSet(gaussianFit, 0);   // added gaussian fit function
+//
+//                    this.getDataGroup().add(dg, iSect, iSide, iComp);
+
                 }
             }
         }
 
     }
 
-    
     // naming convention
     private String nameForObject(String baseName, int sector, int side, int segment) {
         return baseName + "_S" + (sector + 1) + "_L" + (side + 1) + "_P" + (segment + 1);
     }
 
     private String titleForObject(String baseName, int sector, int side, int segment) {
-        return baseName + "S:" + (sector + 1) + " Side: " + (side + 1) + " P: " + (segment + 1);
+        String sideString = "Left";
+        if (side == 1) {
+            sideString = "Right";
+        }
+        return baseName + "S:" + (sector + 1) + sideString + " P: " + (segment + 1);
     }
 
 }
+
